fd9bf2c211ff2e0ab7bb65226f6305df22123372
Jaikrat Singh <tektutorialfeedback@gmail.com>
2021-07-26 22:38:24 +0530

    Initial commit

---
+++ src/main/java/com/spring/solace/consumer/springsolaceconsumer1/SpringSolaceConsumer1Application.java
@@ -0,0 +1,13 @@
+package com.spring.solace.consumer.springsolaceconsumer1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSolaceConsumer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSolaceConsumer1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/solace/consumer/springsolaceconsumer1/SpringSolaceConsumer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.solace.consumer.springsolaceconsumer1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSolaceConsumer1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
